73cf112e7a444b050af37f66dc79e5f32a015d4d
yanagawawa <yukkebbnba@gmail.com>
2021-06-16 13:07:40 +0000

    新規投稿フォーム完成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,24 @@
 class UsersController < ApplicationController
   def show
+    @user = current_user
+    @users = User.find(params[:id])
   end
 
   def index
+
   end
 
   def edit
   end
 
   def update
+    @book = Book.new
   end
+
+   private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+
 end
