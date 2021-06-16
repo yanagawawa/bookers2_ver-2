6c77d93f27fa09d78c65380de749ce277cbd02ee
yanagawawa <yukkebbnba@gmail.com>
2021-06-16 12:18:52 +0000

    index編集中

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
-  #1:nの関係userとbookとの関連付け。関連したuserが消されたらbookも消える 
+
+  #1:nの関係userとbookとの関連付け。関連したuserが消されたらbookも消える
   has_many :books, dependent: :destroy
-  # 画像アップ用のメソッド（attachment）を追加してフィールド名に（image）を指定
-  attachment :image
+  # 画像アップ用のメソッド（attachment）を追加してフィールド名に（profile_image）を指定この時、idは抜く。※refileのルール
+  attachment :profile_image
 end
