05432c722f693304224187a9d0dcbb9aa3a4a3da
yanagawawa <yukkebbnba@gmail.com>
2021-06-17 06:37:00 +0000

    新規投稿の不具合修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,12 @@
 class BooksController < ApplicationController
 
     def create
+        # 何を入れるのか（book_params）を入れないと、空のままviewに送ってしまう
+        @book = Book.new(book_params)
+        # 誰が投稿したのかを識別するために、idを指定
+        @book.user_id = current_user.id
+        @book.save
+        redirect_to book_path(@book)
     end
 
     def index
@@ -25,7 +31,7 @@ class BooksController < ApplicationController
 
     private
 
-    def user_params
+    def book_params
       params.require(:book).permit(:title, :body)
     end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,10 @@ class UsersController < ApplicationController
     @user = current_user
     @users = User.find(params[:id])
     @book = Book.new
+    # このように記述することで、そのユーザ（@users）に関連付けられた投稿のみ、@booksに渡すことができます。
+    # 全体の投稿ではなく、個人が投稿したもののみを表示できます。
     @books = @users.books
+                    # booksはテーブル名
   end
 
   def index
