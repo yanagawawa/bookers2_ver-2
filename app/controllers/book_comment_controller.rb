9f099d9943f95a0a5a0a01157afe214d532ef745
yanagawawa <yukkebbnba@gmail.com>
2021-06-25 08:23:16 +0000

    コメントコントローラーまだかかりそう

--- app/controllers/book_comment_controller.rb
+++ app/controllers/book_comment_controller.rb
@@ -1,9 +1,16 @@
 class BookCommentController < ApplicationController
   def create
-    
+    book = Book.find(params[:id])
+    comment = current_user.book_comments.new(book_comment_params)
   end
   
   def destroy
     
+  end
+  
+  private
+
+  def book_comment_params
+    params.require(:post_comment).permit(:comment)
   end
 end
